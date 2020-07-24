226720a594b41696060be285a990c8dab88cfd8a
Shayne Hartford (ShayBox) <shaybox@shaybox.com>
2020-07-24 16:00:05 -0400

    Add proxy support Add multiple bot and cookie support

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ Simple puppeteer voter for Top.gg
 4. Install dependencies `npm install`
 5. Fill out the config.json file with needed variables
 5. Run `npm start`
+
+## Variables
+`BOT_IDS` An array of Top.GG custom slugs or bot id's
+`COOKIES` An array of Top.GG cookies used to authenticate
+`PROXIES` An array of proxies to use
--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 const puppeteer = require('puppeteer');
-const { BOT_ID, COOKIE } = require('./config.json');
-if (!BOT_ID) throw new Error('BOT_ID not provided');
-if (!COOKIE) throw new Error('COOKIE not provided');
+const { BOT_IDS, COOKIES, PROXIES } = require('./config.json');
+if (!BOT_IDS) throw new Error('BOT_IDS not provided');
+if (!COOKIES) throw new Error('COOKIES not provided');
+if (!PROXIES) throw new Error('PROXIES not provided');
 
-const vote = async () => {
+const vote = async (id, cookie, proxy) => {
 	console.log('Trying to vote');
 
-	const browser = await puppeteer.launch({ headless: false, args: ['--window-size=0,120'] });
+	const args = ['--window-size=1280,720', `--proxy-server=${proxy}`]
+	const browser = await puppeteer.launch({ args, headless: false, ignoreHTTPSErrors: true });
 	const page = (await browser.pages())[0];
 
-	await page.setCookie({ name: 'connect.sid', value: COOKIE, domain: 'top.gg', path: '/' });
-	await page.goto(`https://top.gg/bot/${BOT_ID}/vote`);
+	await page.setCookie({ name: 'connect.sid', value: cookie, domain: 'top.gg', path: '/' });
+	await page.goto(`https://top.gg/bot/${id}/vote`);
 	await page.click('#votingvoted');
 
 	console.log('Successfully voted');
-
 	setTimeout(() => browser.close(), 1000);
-	setTimeout(vote, 1000 * 60 * 60 * 12);
-	console.log('Waiting 12 hours');
 };
-vote();
+
+const main = () => {
+	for (const id of BOT_IDS) {
+		for (const cookie of COOKIES) {
+			vote(id, cookie, PROXIES[Math.floor(Math.random() * PROXIES.length)])
+		}
+	}
+};
+
+main();
