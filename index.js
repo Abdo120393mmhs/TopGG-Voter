9f2277be7c8ed7c1e73cfc5bd895ca96fd5a5be4
Shayne Hartford (ShayBox) <shaybox@shaybox.com>
2020-10-16 21:28:47 -0400

    Make proxies optional Add window size option Add delay before clicking Increase delay before closing Add info to console message Remove useless function

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
-const puppeteer = require('puppeteer');
-const { BOT_IDS, COOKIES, PROXIES } = require('./config.json');
-if (!BOT_IDS) throw new Error('BOT_IDS not provided');
-if (!COOKIES) throw new Error('COOKIES not provided');
-if (!PROXIES) throw new Error('PROXIES not provided');
+const puppeteer = require("puppeteer");
 
-const vote = async (id, cookie, proxy) => {
-	console.log('Trying to vote');
+const { BOT_IDS, COOKIES, PROXIES, WINDOW_SIZE } = require("./config.json");
+if (!BOT_IDS) throw new Error("BOT_IDS not provided");
+if (!COOKIES) throw new Error("COOKIES not provided");
 
-	const args = ['--window-size=1280,720', `--proxy-server=${proxy}`]
+const vote = async (id, cookie, proxy, windowSize) => {
+	console.log("Trying to vote");
+
+	const args = [`--window-size=${windowSize[0] || 1280},${windowSize[1] || 720}`];
+
+	if (proxy) args.push("--proxy-server=" + proxy);
+
 	const browser = await puppeteer.launch({ args, headless: false, ignoreHTTPSErrors: true });
 	const page = (await browser.pages())[0];
 
-	await page.setCookie({ name: 'connect.sid', value: cookie, domain: 'top.gg', path: '/' });
+	await page.setCookie({ name: "connect.sid", value: cookie, domain: "top.gg", path: "/" });
 	await page.goto(`https://top.gg/bot/${id}/vote`);
-	await page.click('#votingvoted');
 
-	console.log('Successfully voted');
-	setTimeout(() => browser.close(), 1000);
+	setTimeout(() => {
+		page.click("#votingvoted");
+
+		console.log(`Successfully voted for ${id} with ${cookie.substr(8)}`);
+
+		setTimeout(() => browser.close(), 1000 * 3);
+	}, 1000 * 60);
 };
 
-const main = () => {
-	for (const id of BOT_IDS) {
-		for (const cookie of COOKIES) {
-			vote(id, cookie, PROXIES[Math.floor(Math.random() * PROXIES.length)]).catch(console.error)
-		}
+for (const id of BOT_IDS) {
+	for (const cookie of COOKIES) {
+		vote(id, cookie, PROXIES[Math.floor(Math.random() * PROXIES.length)], WINDOW_SIZE).catch(console.error);
 	}
-};
-
-main();
+}
